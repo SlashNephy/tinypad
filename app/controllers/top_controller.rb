d448258f239e5d80b3169b6da58b31b308eb3ccd
SlashNephy <spica@starry.blue>
2020-09-07 09:19:09 +0000

    implement kondate listing

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,6 @@
 class TopController < ApplicationController
   def index
+    @recent_kondates = Kondate.order(id: :desc).limit(10)
     @recent_recipes = Recipe.order(id: :desc).limit(10)
     @recent_tsukurepos = Tsukurepo.order(id: :desc).limit(10)
   end
